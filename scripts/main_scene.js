a65c1a4a4fe9d92f0b349622bb2f62b5af61a175
Ryotaro Suzuki <r.suzuki.sys22@morijyobi.ac.jp>
2023-12-19 14:16:17 +0900

    taroとhanakoを配置した

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -10,15 +10,20 @@ class MainScene extends Phaser.Scene {
 
     // シーンの事前読み込み処理
     preload() {
-         // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('back', 'assets/background.png');
+        // 画像の読み込み(使用する時の名前, パス)
+    this.load.image('background', 'assets/background.png');
+    this.load.image('taro', 'assets/taro.png');
+    this.load.image('hanako', 'assets/hanako.png')
     }
-
+   // シーン初期化処理
     create() {
-
-        this.add.image(400, 300, 'back');
-
-}
+            // 単体画像をシーンに追加(X座標,Y座標,画像名)
+        this.add.image(400, 300, 'background');
+        const taro = this.physics.add.sprite(50, 50, 'taro')
+        const hanako = this.physics.add.sprite(750, 400, 'hanako')
+        this.taro = taro
+        this.hanako = hanako
+    }
 
     // 毎フレーム実行される繰り返し処理
 
